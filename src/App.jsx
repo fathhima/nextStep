7a75aaf036a4e4e40696478ee8f2ec3936d07773
fathima sadakkathulla <fathimasadakkathullah@gmail.com>
2026-02-14 09:29:32 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 import Dashboard from "./pages/Dashboard";
 import Jobs from "./pages/Jobs";
-import AddJob from "./pages/Addjob";
+import AddJob from "./pages/AddJobe";
 
 
 export default function App() {
